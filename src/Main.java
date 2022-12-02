0fc00b2376aba24359bb4a898c81e317b83095c4
Максим <gmax7911@gmail.com>
2022-12-02 10:44:33 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Задача №1
+
+        byte variableByte = 7;
+        short variableShort = 5;
+        int variableInteger = 11;
+        long variableLong = 592527;
+        float variableFloat = 4.6f;
+        double variableDouble = 12.806;
+        System.out.println("Значение переменной variableByte типом byte равно " + variableByte);
+        System.out.println("Значение переменной variableShort типом short равно " + variableShort);
+        System.out.println("Значение переменной variableInteger типом int равно " + variableInteger);
+        System.out.println("Значение переменной variableLong типом long равно " + variableLong);
+        System.out.println("Значение переменной variableFloat типом float равно " + variableFloat);
+        System.out.println("Значение переменной variableDouble типом double равно " + variableDouble);
+
+
+    }
+}
